d887d03d789af0ff8d704ffeea053969cbdcc8d5
Petr Chloupek <pchloupek@thetrask.com>
2025-08-04 23:12:05 +0200

    ecs service

--- lesson-7/alb.tf
+++ lesson-7/alb.tf
@@ -10,3 +10,36 @@ resource "aws_lb" "main" {
     Name = "${var.project_name}-alb"
   }
 }
+
+
+resource "aws_lb_target_group" "main" {
+  name     = "${var.project_name}-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = data.aws_vpc.myvpc.id
+  target_type = "ip"
+
+
+  health_check {
+    path                = "/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+
+  tags = {
+    Name = "${var.project_name}-tg"
+  }
+}
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.main.arn
+  }
+}
