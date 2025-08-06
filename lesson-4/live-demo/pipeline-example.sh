0ef049b4bf3b65f7e1227f49fe24f5ebb05faf5f
Petr Chloupek <pchloupek@thetrask.com>
2025-08-06 21:56:33 +0200

    lesson 8, pre-commit

---
+++ lesson-4/live-demo/pipeline-example.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+
+
+cat /secrets/secret.txt >> pipeline.tfvars
--- lesson-7/ecs-service.tf
+++ lesson-7/ecs-service.tf
@@ -6,13 +6,13 @@ resource "aws_ecs_task_definition" "lesson7" {
   family                   = "lesson7"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
-  cpu                      = "256"
-  memory                   = "512"
+  cpu                      = "512"
+  memory                   = "1024"
 
   container_definitions = jsonencode([
     {
       name      = "web"
-      image     = "nginx:latest"
+      image     = "739133790707.dkr.ecr.eu-central-1.amazonaws.com/mynginx:latest"
       portMappings = [
         {
           containerPort = 80
@@ -29,14 +29,14 @@ resource "aws_ecs_service" "lesson7" {
   name            = "lesson7"
   cluster        = aws_ecs_cluster.lesson7.id
   task_definition = aws_ecs_task_definition.lesson7.arn
-  desired_count   = 1
+  desired_count   = 2
 
   launch_type = "FARGATE"
 
   network_configuration {
     subnets          = [data.aws_subnets.ecssubnets.ids[0], data.aws_subnets.ecssubnets.ids[1]]
     security_groups  = [aws_security_group.ecs_tasks.id]
-    assign_public_ip = true
+    assign_public_ip = false
   }
 
     load_balancer {
